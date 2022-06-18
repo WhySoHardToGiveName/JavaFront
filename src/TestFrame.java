ec833ce7daf0c1ecd434ee315b82a2c4ea1c3b91
陈百铭 <957167412@qq.com>
2022-06-19 00:00:23 +0800

    啊啊啊

--- src/TestFrame.java
+++ src/TestFrame.java
@@ -2,6 +2,6 @@ import java.awt.*;
 import java.awt.event.*;
 public class TestFrame {
     public static void main(String[] args) {
-        MainFrame frame = new MainFrame();
+        new MainFrame();
     }
 }
