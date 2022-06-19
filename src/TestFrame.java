1967e88fef5a4a10139aaafabc12c288f8a3e5b9
陈百铭 <957167412@qq.com>
2022-06-19 23:18:01 +0800

    啊啊啊

--- src/TestFrame.java
+++ src/TestFrame.java
@@ -3,6 +3,7 @@ import java.awt.event.*;
 public class TestFrame {
     public static void main(String[] args) {
 //        Login frame = new Login();
-        new StartReserve(15, 5, "场地1", true, true, "18");
+        //new MainFrame("20373324");
+        new StartReserve(5, 2, "场地1", true, true, "18");
     }
 }
